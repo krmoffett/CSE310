65be8ebd4bed3d19a41f7bdc23428c4fb9266ab6
Kyle Moffett <kyle.moffett@hotmail.com>
2016-11-18 08:25:08 -0700

    Initial Setup

---
+++ Project2_Phase2/Project2_Phase2/AdjacencyList.h
@@ -0,0 +1,230 @@
+// Project 2 Phase 2
+// Name: Kyle Moffett
+// ASU Email Address: krmoffet@asu.edu
+// Description: Defines the Graph class with structs for nodes and edges
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Edge {
+	string loser;
+	string score;
+	Edge * next;
+	void printEdge()
+	{
+		cout << loser << score;
+		if (next != NULL)
+			cout << ", ";
+	}
+	void printName()
+	{
+		cout << loser << endl;
+	}
+
+};
+
+struct Node {
+	string name;
+	string color;
+	Node * parent;
+	int d, f;
+	Edge * head;
+	Node()
+	{
+		head = NULL;
+	}
+
+	void printNode()
+	{
+		Edge * p = head;
+		cout << name << " beat: ";
+		if (p == NULL)
+			cout << "none";
+		else
+		{
+			while (p != NULL)
+			{
+				p->printEdge();
+				p = p->next;
+			}
+		}
+	}
+};
+
+class Graph
+{
+private:
+	int v;
+	Node * array;
+	int time;
+public:
+	Graph()		// Constructor
+	{
+		v = 0;
+		array = NULL;
+	}
+	Graph(int v, string names[])	// Overloaded Constructor
+	{
+		this->v = v;
+		array = new Node[v];
+		for (int i = 0; i < v; i++)
+		{
+			array[i].head = NULL;
+			array[i].name = names[i];
+		}
+	}
+	~Graph()	// Destructor
+	{
+		v = 0;
+		delete[] array;
+		time = 0;
+	}
+
+	void graphInsertEdge(string winner, string loser, string score) // Add edge pointing away from winner towards loser
+	{
+		Edge * newEdge = new Edge;
+		newEdge->loser = loser;
+		newEdge->score = score;
+		newEdge->next = NULL;
+		int count = 0;
+		while (array[count].name != winner && count < v)
+			count++;
+		if (count >= v)
+			cout << "cannot find source";
+		else
+		{
+			Edge * current = array[count].head;
+			Edge * previous = NULL;
+
+			while (current != NULL && current->loser < newEdge->loser)
+			{
+				previous = current;
+				current = current->next;
+			}
+			if (previous == NULL)
+			{
+				newEdge->next = array[count].head;
+				array[count].head = newEdge;
+			}
+			else
+			{
+				previous->next = newEdge;
+				newEdge->next = current;
+			}
+		}
+	}
+
+	void printGraph()
+	{
+		cout << "\nGraph (Adjacency Node) contents\n" << endl;
+		if (v == 0)
+			cout << "graph is empty" << endl;
+		else
+		{
+			for (int i = 0; i < v; i++)
+			{
+				array[i].printNode();
+				cout << endl << endl;
+			}
+		}
+	}
+
+	void graphInsertNode(int v, string names[]) // Adds nodes to graph structure
+	{
+		this->v = v;
+		array = new Node[v];
+		for (int i = 0; i < v; ++i)
+		{
+			array[i].head = NULL;
+			array[i].name = names[i];
+		}
+	}
+
+	void depthFirstSearch() // Depth First Search
+	{
+		Node * u = array;
+		for (int i = 0; i < v; i++)		// Initialize all Edges to white and NULL parent
+		{
+			u[i].color = "white";
+			u[i].parent = NULL;
+		}
+
+		time = 0;
+
+		for (int i = 0; i < v; i++)		// Perform visit on undiscovered edges
+		{
+			if (u[i].color == "white")
+			{
+				DFSVISIT(&u[i]);
+			}
+		}
+		printPi();
+		printD();
+		printF();
+	}
+
+	void DFSVISIT(Node * u)		// Helper function for DFS
+	{
+		u->color = "gray";
+		time++;
+		u->d = time;
+		Edge * edge = u->head;
+
+		if (edge != NULL)
+		{
+			do
+			{
+				int count = 0;
+				Node * vee = &array[0];
+				while (vee->name != edge->loser)
+				{
+					count++;
+					vee = &array[count];
+				}
+				if (vee->color == "white")
+				{
+					vee->parent = u;
+					DFSVISIT(vee);
+				}
+				edge = edge->next;
+			} while (edge != NULL);
+		}
+
+		u->color = "black";
+		time++;
+		u->f = time;
+	}
+
+	void printPi()	// Prints the array of pi values for each node
+	{
+		cout << "\nThe array pi contents:\n" << endl;
+		for (int i = 0; i < v; i++)
+		{
+			cout << "pi[" << array[i].name << "] = ";
+			if (array[i].parent == NULL)
+				cout << "none" << endl;
+			else
+				cout << array[i].parent->name << endl;
+		}
+	}
+
+	void printD()	// Prints the discover time for each node
+	{
+		cout << "\nThe array discoverTime contents:\n" << endl;
+		for (int i = 0; i < v; i++)
+		{
+			cout << "discoverTime[" << array[i].name << "] = number " << array[i].d << endl;
+		}
+	}
+
+	void printF()	// Prints the finish time for each node
+	{
+		cout << "\nThe array finishTime contents:\n" << endl;
+		for (int i = 0; i < v; i++)
+		{
+			cout << "finishTime[" << array[i].name << "] = number " << array[i].f << endl;
+		}
+	}
+};
---
+++ Project2_Phase2/Project2_Phase2/Project2P2.cpp
@@ -0,0 +1,149 @@
+// Project 2 Phase 2
+// Name: Kyle Moffett
+// ASU Email Address: krmoffet@asu.edu
+// Description: Driver file for Adjacency List. Takes arguments for 2 file names.
+
+#include <iostream>	
+#include <string>
+#include <fstream>
+#include <sstream>
+#include "AdjacencyList.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	ifstream commandsFile(argv[1]), edgesFile(argv[2]);
+	string command, fill, start, finish, edge, winner, wScore, loser, lScore;
+	int begin, end;
+	Graph g1;
+	bool nameInArray(string name, string array[], int size);
+	void alphaArray(string names[], int size);
+
+	while (command != "quit")
+	{
+		getline(commandsFile, command);
+		cout << "\nnext command: " << command << endl;
+
+		if (command == "print_graph")	// PRINT COMAND
+		{
+			g1.printGraph();
+		}
+		else if (command == "end_graph")	// FREE MEMORY
+		{
+			g1.~Graph();
+		}
+		else if (command == "depth_first_search")	// DFS COMMAND
+		{
+			g1.depthFirstSearch();
+		}
+		else		// GRAPH COMMAND
+		{
+			int numEdges;
+			stringstream ss(command);
+			getline(ss, fill, ',');
+			if (fill == "graph")
+			{
+				getline(ss, start, ',');
+				getline(ss, finish);
+				istringstream(start) >> begin;
+				istringstream(finish) >> end;
+				//begin = stoi(start);
+				//end = stoi(finish);
+				numEdges = end - begin + 1;
+				string * nodeNames = new string[10000];
+				string nameStage1, nameStage2;
+
+				//	EXTRACT NODE NAMES
+				edgesFile.clear();
+				edgesFile.seekg(0, ios::beg);
+				for (int i = 0; i < begin; i++)
+				{
+					getline(edgesFile, fill);
+				}
+
+				int index = 0;
+				for (int i = begin; i <= end; i++)
+				{
+					if (edgesFile.eof() == false)
+					{
+						getline(edgesFile, nameStage1, ',');
+						getline(edgesFile, fill, ',');
+						getline(edgesFile, nameStage2, ',');
+						if (nameInArray(nameStage1, nodeNames, index) == false)
+						{
+							nodeNames[index] = nameStage1;
+							index++;
+						}
+						if (nameInArray(nameStage2, nodeNames, index) == false)
+						{
+							nodeNames[index] = nameStage2;
+							index++;
+						}
+
+						getline(edgesFile, fill);
+					}
+				}
+				string * EdgesToInsert = new string[index];
+
+				for (int i = 0; i < index; i++)
+				{
+					EdgesToInsert[i] = nodeNames[i];
+				}
+
+				alphaArray(EdgesToInsert, index);
+				g1.graphInsertNode(index, EdgesToInsert);
+
+				// EXTRACT EDGES
+				edgesFile.clear();
+				edgesFile.seekg(0, ios::beg);
+				for (int i = 0; i < begin; i++)
+				{
+					getline(edgesFile, fill);
+				}
+				for (int i = begin; i <= end; i++)
+				{
+					if (edgesFile.eof() == false)
+					{
+						getline(edgesFile, winner, ',');
+						getline(edgesFile, wScore, ',');
+						getline(edgesFile, loser, ',');
+						getline(edgesFile, lScore);
+
+						string scoreStr = "(" + wScore + "-" + lScore + ")";
+						g1.graphInsertEdge(winner, loser, scoreStr);
+					}
+				}
+			}
+		}
+	}
+	system("pause");
+	return 0;
+}
+
+bool nameInArray(string name, string array[], int size) // Check if a name is already within a given array
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (name == array[i])
+			return true;
+	}
+	return false;
+}
+
+void alphaArray(string names[], int size) // Sort array alphabetically
+{
+	bool check;
+	do
+	{
+		check = 0;
+		for (int i = 0; i < (size - 1); i++)
+		{
+			if (names[i] > names[i + 1])
+			{
+				names[i].swap(names[i + 1]);
+				check = 1;
+			}
+		}
+	} while (check == 1);
+}
